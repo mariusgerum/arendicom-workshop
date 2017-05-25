63eacf49cd47bce8f802c7c035e788ca159614af
Marius Gerum <me@mariusgerum.de>
2017-05-26 00:52:28 +0200

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# Arendicom PHP workshop
--- application/logic/topics/1-null-coalescing-operators/4-inline-evaluation.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	/**
-	 * Of course null-coalescing operators can be evaluated inline
-	 */
-	
-	echo "Your name is " . ($_GET['name'] ?? "unfortunately unknown") . ".";
-
-?>
